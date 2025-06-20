94d0c3b10b5bfd06b71a296df12265256daf18df
raulmo9t <raulmo9t@gmail.com>
2025-06-20 20:37:04 +0530

    first commit

---
+++ src/constants/stables.js
@@ -0,0 +1,6 @@
+const UPLOAD_FOLDER_BASE_URL = `https://render-blogapp-express.onrender.com/uploads/`;
+const API_BASE_URL = "https://render-blogapp-express.onrender.com";
+
+const stables = { UPLOAD_FOLDER_BASE_URL, API_BASE_URL };
+
+export default stables;
---
+++ src/services/index/posts.js
@@ -0,0 +1,98 @@
+import axios from "axios";
+
+export const getAllPosts = async (
+  searchKeyword = "",
+  page = 1,
+  limit = 10,
+  categories = []
+) => {
+  try {
+    const { data, headers } = await axios.get(
+      `https://render-blogapp-express.onrender.com/api/posts?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}&categories=${categories.join(
+        ","
+      )}`
+    );
+    return { data, headers };
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const getSinglePost = async ({ slug }) => {
+  try {
+    const { data } = await axios.get(
+      `https://render-blogapp-express.onrender.com/api/posts/${slug}`
+    );
+
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message) {
+      throw new Error(error.response.data.message);
+    }
+    throw new Error(error.message);
+  }
+};
+
+export const deletePost = async ({ slug, token }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.delete(
+      `https://render-blogapp-express.onrender.com/api/posts/${slug}`,
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const updatePost = async ({ updatedData, slug, token }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `https://render-blogapp-express.onrender.com/api/posts/${slug}`,
+      updatedData,
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const createPost = async ({ token }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.post(
+      `https://render-blogapp-express.onrender.com/api/posts`,
+      {},
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
